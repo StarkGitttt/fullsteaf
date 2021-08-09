ea2d30766ffd99735147ad0698fbc2d70fc5abfc
Phan Tan Loc <lcphan512@gmail.com>
2021-08-09 21:57:28 +0700

    Add app.js file

--- responsive-travel-website-main/assets/js/app.js
+++ responsive-travel-website-main/assets/js/app.js
@@ -1,9 +1,17 @@
+// Account Admin
 var CORRECT_USER = 'titanic@gmail.com';
 var CORRECT_PASS = '123456';
 var inputUser = document.getElementById('email');
 var inputPassword = document.getElementById('password');
-
+// Account User
+var userName = "lcphan512@gmail.com";
+var userPassword = "123456";
+// Form login
 var formLogin = document.getElementById('form-2');
+// Get Role
+var role = document.querySelector('.nav__item.role');
+var getBtn = role.querySelector('.nav__link');
+// Hide Form
 
 if (formLogin.attachEvent) {
     formLogin.attachEvent('submit', onFormSubmit);
@@ -15,11 +23,16 @@ function onFormSubmit(e) {
     var username = inputUser.value;
     var password = inputPassword.value;
     if (username == CORRECT_USER && password == CORRECT_PASS) {
-        alert("Đăng nhập thành công!! Chào Admin")
-
-
-
-    } else {
+        alert("Đăng nhập thành công!! Chào Admin");
+        role.classList.add('show');
+        getBtn.innerHTML = 'ADMIN'
+        // hideSigninForm();
+    }else if(username == userName && password == userPassword){
+        alert("Đăng nhập thành công!! Chào User");
+        role.classList.add('show');
+        getBtn.innerHTML = 'User'
+        // hideSigninForm();
+    }else {
         alert("Thất bại trong quá trình đăng nhập!!!")
     }
 }
